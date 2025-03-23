a4c6fec92f448b1b7171c8792af6be497e1e78d4
minsungkang <minsungkang2012@gmail.com>
2025-03-23 16:09:04 +0900

    first commit

--- InheritedClassPracticeApplication.java
+++ InheritedClassPracticeApplication.java
@@ -1,6 +1,6 @@
 import animalclass.*;
 
-public class AdvancedClassPracticeApplication {
+public class InheritedClassPracticeApplication {
     public static void main(String[] args) {  // pvsm tab
         Dog myDog = new Dog();
         myDog.name = "Coco";  // name is public
